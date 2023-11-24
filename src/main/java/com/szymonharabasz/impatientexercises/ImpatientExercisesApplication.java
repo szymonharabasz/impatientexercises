b245456308a0e87cc51569d7bf2ecef964063dbc
Szymon Harabasz <szymon.harabasz@gmail.com>
2023-11-24 14:16:09 +0100

    Initial commit

---
+++ src/main/java/com/szymonharabasz/impatientexercises/ImpatientExercisesApplication.java
@@ -0,0 +1,13 @@
+package com.szymonharabasz.impatientexercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpatientExercisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpatientExercisesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/szymonharabasz/impatientexercises/ImpatientExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szymonharabasz.impatientexercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpatientExercisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
